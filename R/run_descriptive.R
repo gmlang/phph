2387a3ca6551114ec911ee9946561af14cb66eec
gmlang <rigor6vigor@gmail.com>
2015-05-31 22:37:03 -0400

    no message

--- R/run_descriptive.R
+++ R/run_descriptive.R
@@ -11,7 +11,11 @@ run_descriptive = function() {
         
         # make boxplot
         plt = ezplot::mk_boxplot(dat)
-        p = plt("RT", "TTOBRC", ylab="Time to Breast Cancer Occurrence", legend=F)
+#         p = plt("RT", "TTOBRC", ylab="Time to Breast Cancer Occurrence", 
+#                 legend=F)
+        p = plt("RT", "TTOBRC", main="test", 
+                legend=F)
+
         print(p)
         
         # create data.frame to hold plots title and index
